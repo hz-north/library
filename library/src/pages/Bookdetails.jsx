40e5f1f322f8df3f9c3ed1bd99fe47eaad1658b3
morty-9 <hzawnineseven@gmail.com>
2024-06-21 16:33:13 +0630

    bookDetail

--- library/src/pages/Bookdetails.jsx
+++ library/src/pages/Bookdetails.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import useFetch from "../hooks/useFetch";
-
+import bookImg from '../assets/images.png';
 export default function Bookdetails() {
   let { id } = useParams();
   let { data: book, loading, error } = useFetch(`http://localhost:3000/books/${id}`)
@@ -9,7 +9,24 @@ export default function Bookdetails() {
       {error && <p>{error}</p>}
       {loading && <p>loading ....</p>}
       {book && (
-        <h1>{book.title}</h1>
+       <div className="w-full flex">
+          <div className="w-1/2 bg-slate-100 p-5 ">
+            <img src={bookImg} alt=""  className="w-[80%] mx-auto"/>
+          </div>
+          <div className="m-5">
+            <h1>{book.title}</h1>
+            <div className="my-4 ">
+            {book.categories.map(category => (
+              <span className='mx-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500 ' key={category}>
+                {category}
+              </span>
+            ))}
+            </div>
+            <p>
+              {book.description}
+            </p>
+          </div>
+        </div>
       )}
     </>
   ) 
